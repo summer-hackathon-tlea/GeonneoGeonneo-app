f9ca2e2ceb79ff5d66aa17efae5cb8227733bcd3
gimhanul <gimhanuly@gmail.com>
2022-07-14 17:30:27 +0900

    ADD :: Login
    - login screen 구현
    - login api와 연동
    - axios, AsyncStorage 추가
    - AsyncStorage에 accesstoken 저장

---
+++ api/url.js
@@ -0,0 +1,1 @@
+export const baseUrl = "http://10.150.150.191:8080";
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,10 +3,26 @@ import { Image, StyleSheet, View } from "react-native";
 import CustomInput from "../components/CustomInput";
 import Logo from "../assets/logo.png";
 import CustomButton from "../components/Button";
+import { baseUrl } from "../api/url";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function LoginScreen() {
+export default function LoginScreen({ navigation }) {
   const [dongho, setDongho] = useState("");
   const [password, setPassword] = useState("");
+
+  const loginUser = async () => {
+    try {
+      const response = await axios.post(`${baseUrl}/user/login`, {
+        "dongho": dongho,
+        "password": password
+      });
+      await AsyncStorage.setItem("access-token", response.data.accessToken);
+      navigation.navigate("Home");
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   return (
     <View style={styles.root}>
@@ -30,8 +46,7 @@ export default function LoginScreen() {
         <View style={styles.loginButton}>
           <CustomButton
             title="로그인"
-            willDo={() => {
-            }}
+            onPress={loginUser}
           />
         </View>
       </View>
@@ -45,7 +60,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   logoWrapper: {
-    flex: 1.3,
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -57,7 +72,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   loginButton: {
-    flex: 1,
+    flex: 1.5,
     alignItems: "center",
   },
 });
